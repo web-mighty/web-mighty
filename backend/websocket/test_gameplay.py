76e1e07955b2cc561c7348f1b8cb3134b8cb31c6
skystar-p <wogus150@naver.com>
2017-12-02 03:11:44 +0900

    gameplay test template

---
+++ backend/websocket/test_gameplay.py
@@ -0,0 +1,8 @@
+from channels.test import ChannelTestCase, WSClient
+from api.models import create_user
+from django.contrib.auth.models import User
+from django.core.cache import cache
+
+
+class GameplayTest(ChannelTestCase):
+    pass
