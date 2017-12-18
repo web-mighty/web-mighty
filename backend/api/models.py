48f7e3e568ffd8397df9581aa22e83e037a74cbe
skystar-p <wogus150@naver.com>
2017-12-19 06:29:34 +0900

    index Room by room_id

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,7 +11,7 @@ import os
 
 
 class Room(models.Model):
-    room_id = models.CharField(max_length=40, unique=True)
+    room_id = models.CharField(max_length=40, unique=True, db_index=True)
     title = models.CharField(max_length=100)
     is_private = models.BooleanField()
     password = models.CharField(max_length=120)
